5f4909e7477b778a50d00f9079871fa51299adcd
Mazharul Islam <505mazharulislam@gmail.com>
2024-02-27 00:34:12 +0600

    login page crate

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,44 @@
+import { Link } from "react-router-dom";
 import Navbar from "../shared/Navbar/Navbar";
 
 const Login = () => {
+
+    const handleLogin = e => {
+        e.preventDefault();
+        console.log(e.currentTarget);
+        const form = new FormData(e.currentTarget);
+        console.log(form.get("email"));
+      }
+      
+
     return (
         <div>
             <Navbar></Navbar>
-            <h2>This is login</h2>
+            <div className="mx-auto">
+                <h2 className="text-center">Login your account</h2>
+
+                <form onSubmit={handleLogin} className=" md:w-3/4 lg:w-1/2 mx-auto card-body">
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text text-lg font-bold">Email Address</span>
+                        </label>
+                        <input type="email" name="email" placeholder="Enter your email address" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text  text-lg font-bold">Password</span>
+                        </label>
+                        <input type="password" placeholder="Enter your Password" className="input input-bordered" required />
+                        <label className="label">
+                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                        </label>
+                    </div>
+                    <div className="form-control mt-6">
+                        <button className="btn bg-[#403F3F] text-white ">Login</button>
+                    </div>
+                </form>
+                <p className="text-center -mt-3">Don't have an account <Link className="text-[#F75B5F]" to='/register'>Register</Link> </p>
+            </div>
         </div>
     );
 };
