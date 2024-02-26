9ae7817108d1c7517d59de2df0e0b768c53d9d81
Mazharul Islam <505mazharulislam@gmail.com>
2024-02-26 23:25:55 +0600

    right side navbar login section and find us section crate

--- src/pages/shared/RightSideNav/RightSideNav.jsx
+++ src/pages/shared/RightSideNav/RightSideNav.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import { FaFacebook, FaGithub, FaGoogle, FaInstagram, FaTwitter } from "react-icons/fa";
 
 const RightSideNav = () => {
     return (
         <div>
-            Right side 
+
+            <div className='p-4 space-y-3 mb-6'>
+                <h2 className='text-3xl font-medium'>Login With</h2>
+
+                <button className="btn gap-4 ">
+                <FaGoogle />
+                    Login With Google
+                </button>
+                <button className="btn gap-4 ">
+                <FaGithub />
+                    Login With GitHub
+                </button>
+            </div>
+
+            <div className='p-4  mb-6'>
+                <h2 className='text-3xl  font-medium'>Find Us On</h2>
+
+                <a className="btn gap-4 text-lg border">
+                <FaFacebook />
+                    FaceBook
+                </a>
+                <br />
+                <a className="btn gap-4 text-lg">
+                <FaTwitter />
+                Twitter
+                </a>
+                <br />
+                <a className="btn gap-4 text-lg">
+                <FaInstagram />
+                    Instagram
+                </a>
+              
+            </div>
+
         </div>
     );
 };
