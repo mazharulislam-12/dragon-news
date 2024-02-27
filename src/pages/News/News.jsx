72300f2ca6fc882754746bd5c94d537a7f2d7420
Mazharul Islam <505mazharulislam@gmail.com>
2024-02-27 20:53:18 +0600

    left side navbar image section crate

--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -2,10 +2,19 @@ import { useParams } from 'react-router-dom';
 import Header from '../shared/Header/Header';
 import RightSideNav from '../shared/RightSideNav/RightSideNav';
 import Navbar from '../shared/Navbar/Navbar.jsx'
+import { useEffect, useState } from 'react';
 
 const News = () => {
 
-    const {_id} = useParams();
+    const { _id } = useParams();
+
+    const [newsData, setNewsData] = useState([])
+
+    useEffect(() => {
+        fetch('/news.json')
+            .then(res => res.json())
+            .then(data => setNewsData(data) )
+    }, [])
 
     return (
         <div>
@@ -15,7 +24,6 @@ const News = () => {
             <div className='grid mx-auto md:grid-cols-4'>
                 <div className='col-span-3'>
                     <h2>Dragon News  </h2>
-                    <p>{_id}</p>
                     
 
                 </div>
--- src/pages/shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/shared/LeftSideNav/LeftSideNav.jsx
@@ -2,6 +2,10 @@ import { data } from 'autoprefixer';
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
+import img1 from '../../../assets/1.png'
+import img2 from '../../../assets/2.png'
+import img3 from '../../../assets/3.png'
+
 const LeftSideNav = () => {
 
     const [categories, setCategories] = useState([]);
@@ -10,14 +14,55 @@ const LeftSideNav = () => {
         fetch('./categories.json')
             .then(res => res.json())
             .then(data => setCategories(data))
-    }, [])
+    }, []);
+
+
 
     return (
         <div>
             <h2>All Caterogy</h2>
             {
-                categories.map(category => <Link to={`/category/${category.id}`} className='block mb-4 text-xl' key={category.id}>{category.name}</Link> )
+                categories.map(category => <Link to={`/category/${category.id}`} className='block mb-4 text-xl' key={category.id}>{category.name}</Link>)
             }
+
+            <div className='mt-12'>
+                <img src={img1} alt="" />
+                <h3>Bayern Slams Authorities Over Flight Delay to Club World Cup</h3>
+                <div className='flex items-center justify-around mx-auto'>
+                    <span>Sports</span>
+                    <span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                        <path d="M6.75 3V5.25M17.25 3V5.25M3 18.75V7.5C3 6.90326 3.23705 6.33097 3.65901 5.90901C4.08097 5.48705 4.65326 5.25 5.25 5.25H18.75C19.3467 5.25 19.919 5.48705 20.341 5.90901C20.7629 6.33097 21 6.90326 21 7.5V18.75M3 18.75C3 19.3467 3.23705 19.919 3.65901 20.341C4.08097 20.7629 4.65326 21 5.25 21H18.75C19.3467 21 19.919 20.7629 20.341 20.341C20.7629 19.919 21 19.3467 21 18.75M3 18.75V11.25C3 10.6533 3.23705 10.081 3.65901 9.65901C4.08097 9.23705 4.65326 9 5.25 9H18.75C19.3467 9 19.919 9.23705 20.341 9.65901C20.7629 10.081 21 10.6533 21 11.25V18.75" stroke="#9F9F9F" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg></span>
+                    <span>Jan 4, 2022</span>
+                </div>
+            </div>
+
+            <div className='mt-12'>
+                <img src={img2} alt="" />
+                <h3>Bayern Slams Authorities Over Flight Delay to Club World Cup</h3>
+                <div className='flex items-center justify-around mx-auto'>
+                    <span>Sports</span>
+                    <span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                        <path d="M6.75 3V5.25M17.25 3V5.25M3 18.75V7.5C3 6.90326 3.23705 6.33097 3.65901 5.90901C4.08097 5.48705 4.65326 5.25 5.25 5.25H18.75C19.3467 5.25 19.919 5.48705 20.341 5.90901C20.7629 6.33097 21 6.90326 21 7.5V18.75M3 18.75C3 19.3467 3.23705 19.919 3.65901 20.341C4.08097 20.7629 4.65326 21 5.25 21H18.75C19.3467 21 19.919 20.7629 20.341 20.341C20.7629 19.919 21 19.3467 21 18.75M3 18.75V11.25C3 10.6533 3.23705 10.081 3.65901 9.65901C4.08097 9.23705 4.65326 9 5.25 9H18.75C19.3467 9 19.919 9.23705 20.341 9.65901C20.7629 10.081 21 10.6533 21 11.25V18.75" stroke="#9F9F9F" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg></span>
+                    <span>Jan 4, 2022</span>
+                </div>
+            </div>
+
+            <div className='mt-12'>
+                <img src={img3} alt="" />
+                <h3>Bayern Slams Authorities Over Flight Delay to Club World Cup</h3>
+                <div className='flex items-center justify-around mx-auto'>
+                    <span>Sports</span>
+                    <span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                        <path d="M6.75 3V5.25M17.25 3V5.25M3 18.75V7.5C3 6.90326 3.23705 6.33097 3.65901 5.90901C4.08097 5.48705 4.65326 5.25 5.25 5.25H18.75C19.3467 5.25 19.919 5.48705 20.341 5.90901C20.7629 6.33097 21 6.90326 21 7.5V18.75M3 18.75C3 19.3467 3.23705 19.919 3.65901 20.341C4.08097 20.7629 4.65326 21 5.25 21H18.75C19.3467 21 19.919 20.7629 20.341 20.341C20.7629 19.919 21 19.3467 21 18.75M3 18.75V11.25C3 10.6533 3.23705 10.081 3.65901 9.65901C4.08097 9.23705 4.65326 9 5.25 9H18.75C19.3467 9 19.919 9.23705 20.341 9.65901C20.7629 10.081 21 10.6533 21 11.25V18.75" stroke="#9F9F9F" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg></span>
+                    <span>Jan 4, 2022</span>
+                </div>
+            </div>
+
+
+
         </div>
     );
 };
